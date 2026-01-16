1468550a59a4eb5a5999ba06b7b7dabb7bd3ee1f
𝐆𝐇𝐎𝐒𝐓 𝐊𝐈𝐍𝐆 <hi2879639@gmail.com>
2026-01-16 18:00:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups
 MENTION_REPLY: process.env.MENTION_REPLY || "true",
 // make true if want auto voice reply if someone menetion you
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/kiy0hl.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://h.uguu.se/PhgJmabv.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
@@ -40,7 +40,7 @@ OWNER_NAME: process.env.OWNER_NAME || "GHOST",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "© popkid xtr bot",
 // add bot owner name
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n8o8py.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://h.uguu.se/PhgJmabv.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || ">Ꮲhᴀnᴛᴏʍ Ꮱ IS ALIVE😍",
 // add alive msg here
